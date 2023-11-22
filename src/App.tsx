6477e6b767b864d6c62948356abd45c3f9a49944
MITHUN MOHANDAS <mithunsruthi@gmail.com>
2023-11-22 11:07:53 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <>
+      <h1>Sample Project</h1>
+      <p> Created by <a href="https://mithunmohandas2.github.io/Mithun-Mohandas/"> Mithun Mohandas </a> </p>
+    </>
+  )
+}
+
+export default App
