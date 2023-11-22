76aee2cb38f899792fbc62fd3c19f9df25537245
MITHUN MOHANDAS <mithunsruthi@gmail.com>
2023-11-22 12:44:52 +0530

    tailwind linked

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ function App() {
     <>
       <h1>Sample Project</h1>
       <p> Created by <a href="https://mithunmohandas2.github.io/Mithun-Mohandas/"> Mithun Mohandas </a> </p>
+
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </>
   )
 }
